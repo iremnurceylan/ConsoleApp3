1253859a91ccc6f65c79d94f7a49769873e7e7bd
irem nur ceylan <ceylannnirem@outlook.com>
2021-01-17 15:00:41 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kurslar = new string[] { "Yazılım kursu", "java kursu", "pyhton kursu" };
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+           
+            Console.WriteLine("for bitti!");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+        }
+    }
+}
